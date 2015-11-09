3ba2f70ab132449f583b7beba07530449d3bc713
Vladyslav <amberv.mail@gmail.com>
2015-11-09 17:34:07 +0100

    updated readme

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,15 @@
-#usage example:
-# x < - makeCacheMatrix()
+# usage example:
+# x <- makeCacheMatrix()
 # cacheSolve(x, m = matrix(1:4, 2, 2)) #will calculate inverse
 # cacheSolve(x, m = matrix(1:4, 2, 2)) #run once again, will show a message about cache usage
-# cacheSolve(x, m = matrix(2:4, 2, 2)) #changing matrix will cause recalculation
-# cacheSolve(x, m = matrix(2:4, 2, 2)) #but run again, no recalculation
+# cacheSolve(x, m = matrix(2:5, 2, 2)) #changing matrix will cause recalculation
+# cacheSolve(x, m = matrix(2:5, 2, 2)) #but run again, no recalculation
+
+#another way to do this:
+# x <- makeCacheMatrix(matrix(1:4, 2, 2))
+# cacheSolve(x) #will calculate inverse
+# cacheSolve(x) #run once again, will show a message about cache usage
+
 
 ## constructs an object needed for the cacheSolve function, has one optional parameter where the matrix can be passed
 
